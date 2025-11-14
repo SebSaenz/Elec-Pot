f1c1429e2587c78fe843268aa6b8b033023d4d65
Alvaro-23-N <alvaronegreteb@gmail.com>
2025-11-14 10:51:20 -0500

    ss

---
+++ Modulo 7 Single Phase Inverter with Digital PWM/Simulation/COD_arduino2/INVERTER2.ino
@@ -0,0 +1,37 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7, HIGH);
+  delayMicroseconds(800);
+  digitalWrite(7, LOW);
+  delayMicroseconds(1100);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(2100);
+  digitalWrite(7, LOW);
+  delayMicroseconds(500);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(2300);
+  digitalWrite(7, LOW);
+  delayMicroseconds(700);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(1900);
+  digitalWrite(7, LOW);
+  delayMicroseconds(1400);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(1100);
+  digitalWrite(7, LOW);
+  delayMicroseconds(2100);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(7, LOW);
+  delayMicroseconds(2300);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(700);
+  digitalWrite(7, LOW);
+  delayMicroseconds(1900);
+
+}
---
+++ Modulo 7 Single Phase Inverter with Digital PWM/Simulation/COD_arduino3/INVERTER3.ino
@@ -0,0 +1,47 @@
+float t1=859;
+float t2=1351.7;
+float t3=2562;
+float t4=2744.6;
+float d1=t2-t1;
+float d2=t3-t2;
+float d3=t4-t3;
+float d4=10000-(2^t4);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7, HIGH);
+  delayMicroseconds(t1);
+  digitalWrite(7, LOW);
+  delayMicroseconds(d1);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(d2);
+  digitalWrite(7, LOW);
+  delayMicroseconds(d3);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(d4);
+  digitalWrite(7, LOW);
+  delayMicroseconds(d3);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(d2);
+  digitalWrite(7, LOW);
+  delayMicroseconds(d1);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(t1);
+  digitalWrite(7, LOW);
+  delayMicroseconds(t1);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(d1);
+  digitalWrite(7, LOW);
+  delayMicroseconds(d2);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(d3);
+  digitalWrite(7, LOW);
+  delayMicroseconds(d4);
+  digitalWrite(7, HIGH);
+
+}
